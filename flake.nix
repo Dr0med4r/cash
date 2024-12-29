9b2e84d44ef23c07466aa7c631ad3447d400ce24
Dr0med4r <trompeter-stoll@gmx.de>
2024-12-29 18:57:23 +0100

    add package itself to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,41 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          gcc
-          flex
-          bison
-          readline
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inputs = [
+          pkgs.gcc
+          pkgs.flex
+          pkgs.bison
+          pkgs.readline
         ];
-      };
-    }
-  );
+      in {
+        packages = {
+          default = self.packages.${system}.cash;
+          cash = pkgs.stdenv.mkDerivation {
+            pname = "cash";
+            version = "1.0";
+
+            src = ./.;
+
+            nativeBuildInputs = inputs;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp cash $out/bin
+            '';
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = inputs;
+        };
+      }
+    );
 }
