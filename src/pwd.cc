0d7c074d9d8723ef90b461963fcdca8c1e0b66d8
Dr0med4r <trompeter-stoll@gmx.de>
2025-01-07 13:51:39 +0100

    remove quotation from pwd

--- src/pwd.cc
+++ src/pwd.cc
@@ -4,6 +4,6 @@
 
 void ShellBuiltinPwd::exec(int fd1, int fd2) {
     setup_fds(fd1, fd2);
-    std::cout << std::filesystem::current_path() << "\n";
+    std::cout << std::filesystem::current_path().string() << "\n";
     exit(0);
 }
